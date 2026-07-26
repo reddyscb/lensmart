5d7771b10c65636f72239ae071de685dc000a1c8
reddyscb <bsreecharanreddy@gmail.com>
2026-07-26 12:32:27 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = "murali-v1";
+const ASSETS = ["./Murali.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      Promise.all(
+        ASSETS.map((url) =>
+          fetch(url).then((res) => { if (res.ok) return cache.put(url, res); }).catch(() => {})
+        )
+      )
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request)
+        .then((res) => {
+          if (res.ok && event.request.method === "GET") {
+            const clone = res.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, clone));
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
